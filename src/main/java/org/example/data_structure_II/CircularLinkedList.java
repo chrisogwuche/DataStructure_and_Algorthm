2227275e1dbbd861157370dc95c7b3bc09a75bcd
Chris Ogwuche <ogwuchechris@gmail.com>
2023-06-15 22:37:34 +0100

    updated

--- src/main/java/org/example/data_structure_II/CircularLinkedList.java
+++ src/main/java/org/example/data_structure_II/CircularLinkedList.java
@@ -26,15 +26,93 @@ public class CircularLinkedList {
         cLinked.head.next = second;
         second.next = third;
         third.next = cLinked.tail;
-        cLinked.tail.next = cLinked.head;
+//        cLinked.tail.next = cLinked.head;
 
 
         // using  while(cLinked.head !=null) without an index bound condition will give an infinity loop because they are all connected
-        int  i = 6;
-        while(i > 0){
-            System.out.println("values : " +cLinked.head.value);
-            cLinked.head = cLinked.head.next;
-            i--;
+//        int  i = 6;
+//        while(i > 0){
+//            System.out.println("values : " +cLinked.head.value);
+//            cLinked.head = cLinked.head.next;
+//            i--;
+//        }
+
+        //INSERT AT FIRST
+//        Node inserted = insertAtFirst(cLinked.head, 9);
+//
+//        while(inserted != null){
+//            System.out.println("inserted at first function: " +inserted.value);
+//            inserted = inserted.next;
+//        }
+//
+        //INSERT AT LAST
+//        Node x= insertAtLast(cLinked.head, 8);
+//        while(x!= null){
+//            System.out.println("inserted at first function: " +x.value);
+//            x = x.next;
+//        }
+
+
+        //INSERT AFTER
+//        Node x= insertAfter(cLinked.head, 8);
+//        while(x!= null){
+//            System.out.println("inserted at first function: " +x.value);
+//            x = x.next;
+//        }
+
+
+        //INSERT DELETE NODE
+        Node x= deleteNode(cLinked.head, 2);
+        while(x!= null){
+            System.out.println("inserted at first function: " +x.value);
+            x = x.next;
         }
+
+
+    }
+
+    public static Node insertAtFirst(Node head, int value){
+        Node newNode = new Node(value);
+
+        newNode.next = head;
+        head = newNode;
+
+        return head;
+    }
+
+    public static Node insertAtLast(Node head, int value){
+       Node newNode = new Node(value);
+       newNode.next = null;
+       Node last = head;
+
+       while (last.next != null){
+           last = last.next;
+       }
+       last.next = newNode;
+
+       return last;
+    }
+
+    public static Node insertAfter(Node head, int value){
+        Node nextNode = new Node(value);
+
+        nextNode.next = head.next;
+        head. next = nextNode;
+        return head;
+    }
+
+    public static Node deleteNode(Node head, int position){
+
+        Node temp = head;
+
+        for(int i=0; i <= position-2; i++){
+            System.out.println("--" + temp.value);
+            temp = temp.next;
+
+        }
+        Node n = temp.next.next;
+        temp.next = n;
+
+        return temp;
     }
 }
